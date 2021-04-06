f10a4ff8130493f680b01a79949d484eed2d76e8
thaonguyen19 <thachthao2605@gmail.com>
2021-04-06 15:29:09 -0700

    State the exact 112 attributes used

--- CUB/generate_new_data.py
+++ CUB/generate_new_data.py
@@ -67,6 +67,11 @@ def get_class_attributes_data(min_class_count, out_dir, modify_data_dir='', keep
     Use train.pkl to aggregate attributes on class level and only keep those that are predominantly 1 for at least min_class_count classes
     Transform data in modify_data_dir file using the class attribute statistics and save the new dataset to out_dir
     If keep_instance_data is True, then retain the original values of the selected attributes. Otherwise save aggregated class level attributes
+    In our paper, we set min_class_count to be 10 and only use the following 112 attributes of indices 
+    [1, 4, 6, 7, 10, 14, 15, 20, 21, 23, 25, 29, 30, 35, 36, 38, 40, 44, 45, 50, 51, 53, 54, 56, 57, 59, 63, 64, 69, 70, 72, 75, 80, 84, 90, 91, \
+    93, 99, 101, 106, 110, 111, 116, 117, 119, 125, 126, 131, 132, 134, 145, 149, 151, 152, 153, 157, 158, 163, 164, 168, 172, 178, 179, 181, \
+    183, 187, 188, 193, 194, 196, 198, 202, 203, 208, 209, 211, 212, 213, 218, 220, 221, 225, 235, 236, 238, 239, 240, 242, 243, 244, 249, 253, \
+    254, 259, 260, 262, 268, 274, 277, 283, 289, 292, 293, 294, 298, 299, 304, 305, 308, 309, 310, 311]
     """
     data = pickle.load(open('train.pkl', 'rb'))
     class_attr_count = np.zeros((N_CLASSES, N_ATTRIBUTES, 2))
